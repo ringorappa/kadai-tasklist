1041d5c5684d24a49e1b5625deaaea4240db99c5
airi.iwama <airxxxx1209@gmail.com>
2017-07-26 05:38:15 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    set_task
   end
 
   def new
@@ -24,12 +26,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -40,11 +39,14 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
+  end
+  
+  def set_task
+    @task = Task.find(params[:id])
   end
   
   def task_params
